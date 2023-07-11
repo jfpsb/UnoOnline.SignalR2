531b3ca4f8ca82c4775fa3435e5e9d3f64433804
Felipe Borges <jfpsb_felipe2@hotmail.com>
2023-07-10 23:06:07 -0300

    adicionando comentários fechando tela de escolher cor se passar a vez

--- Hubs/UnoOnlineHub.cs
+++ Hubs/UnoOnlineHub.cs
@@ -6,20 +6,21 @@ namespace UnoOnline.SignalR2.Hubs
 {
     public class UnoOnlineHub : Hub
     {
-        private static StatusPartida? statusPartida;
-        private System.Timers.Timer timerJogada;
-        private static DateTime expiraEm;
-        private DateTime inicioJogada;
+        private static StatusPartida statusPartida; //Guarda os dados da partida
+        private System.Timers.Timer timerJogada; //Timer para jogada
+        private static DateTime expiraEm; //Guarda quando a vez do jogador atual expira
         private string _segundosAteExpiracaoEmString;
-        private int segundosTimer = 15;
+        private int segundosTimer = 15; //Segundos máximo para cada jogador fazer sua jogada
         internal static IHubCallerClients MyClients { get; set; }
 
+        /// <summary>
+        /// Ao receber nova conexão de cliente, caso seja o primeiro jogador instancio o status da partida e gero o baralho.
+        /// </summary>
         public override Task OnConnectedAsync()
         {
             if (statusPartida == null)
             {
                 statusPartida = new StatusPartida();
-                statusPartida.GeraBaralho();
             }
             MyClients ??= Clients;
             return base.OnConnectedAsync();
@@ -37,6 +38,12 @@ namespace UnoOnline.SignalR2.Hubs
         }
         public async Task EnviaUltimaCartaJogada(Carta carta, Jogador jogador)
         {
+            //Testo se quem fez a jogada foi o jogador da vez ou se houve corte
+            if (jogador.Uuid != statusPartida.JogadorDaVez.Uuid)
+            {
+                await EnviaEventosPartida($"{jogador.Nome} realizou um corte e {statusPartida.JogadorDaVez.Nome} perdeu a vez.");
+            }
+
             //Recupero a carta atualmente jogada por último
             var ultCartaJogada = statusPartida.Baralho[statusPartida.UltimaCarta.Uuid];
             ultCartaJogada.EstaEmBaralho = true;
@@ -44,8 +51,8 @@ namespace UnoOnline.SignalR2.Hubs
             //Ultima carta no status recebe a nova carta que foi jogada
             statusPartida.UltimaCarta = carta;
 
-            var jg = statusPartida.Jogadores.Where(w => w.Uuid == jogador.Uuid).First();
-            var novaCarta = statusPartida.Baralho[carta.Uuid];
+            var jg = statusPartida.Jogadores.Where(w => w.Uuid == jogador.Uuid).First(); //Retorno instancia de jogador no servidor
+            var novaCarta = statusPartida.Baralho[carta.Uuid]; //Retorno instancia de carta no servidor
             novaCarta.Cor = carta.Cor;
 
             //Testa se jogador gritou Uno
@@ -59,15 +66,17 @@ namespace UnoOnline.SignalR2.Hubs
             var currentNode = statusPartida.Jogadores.Find(jg);
             currentNode.Value.Cartas.Remove(novaCarta);
 
+            //Se após jogar a carta tiver zero cartas este jogador é o vencedor
             if (currentNode.Value.Cartas.Count == 0)
             {
                 await EnviaEventosPartida($"Jogador {jogador.Nome} venceu a partida.");
                 await Clients.All.SendAsync("AcabouPartida", jogador);
+                statusPartida = new StatusPartida();
                 DestroiTimer();
                 return;
             }
 
-            var proxJg = statusPartida.RetornaProximoJogador(jg);
+            var proxJg = statusPartida.RetornaProximoJogador(jg); //Retorno próximo jogador da sequência
 
             switch (novaCarta.Tipo)
             {
@@ -128,7 +137,7 @@ namespace UnoOnline.SignalR2.Hubs
                 return;
             }
 
-            foreach (var item in statusPartida.RetornaCartasDoBaralho(3, false))
+            foreach (var item in statusPartida.RetornaCartasDoBaralho(7, false))
             {
                 jogador.Cartas.Add(item);
             }
@@ -167,20 +176,17 @@ namespace UnoOnline.SignalR2.Hubs
             else
             {
                 var horaAtual = DateTime.Now.TimeOfDay;
-                var timeSpan1 = new TimeSpan(inicioJogada.Hour, inicioJogada.Minute, inicioJogada.Second);
                 var timeSpan2 = new TimeSpan(expiraEm.Hour, expiraEm.Minute, expiraEm.Second);
 
-                if (horaAtual >= timeSpan1 && horaAtual <= timeSpan2)
+                if (horaAtual <= timeSpan2)
                 {
                     var timeSpanRestante = timeSpan2.Subtract(horaAtual);
                     SegundosAteExpiracaoEmString = timeSpanRestante.ToString(@"ss\.f");
                 }
             }
 
-            Console.WriteLine(expiraEm);
             await MyClients.All.SendAsync("AtualizarTimer", SegundosAteExpiracaoEmString);
         }
-
         public string SegundosAteExpiracaoEmString
         {
             get
